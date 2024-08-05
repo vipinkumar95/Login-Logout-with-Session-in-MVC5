34397f5fc9bb89d1b3957c2a11a93332ac162f2a
Rudra Pal <Rudra Pal@DESKTOP-EPCRK2D>
2024-08-05 12:24:58 +0530

    Add project files.

---
+++ LoginMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["Username"] == null)
+            {
+                return RedirectToAction("Index","Login");
+            }
+            return View();
+        }
+
+       
+    }
+}
---
+++ LoginMVC/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        MvcLoginEntities1 db = new MvcLoginEntities1();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(logindb logindb)
+        {
+            if (ModelState.IsValid) 
+            {
+                var data =  db.logindbs.Where(x => x.Username.Equals(logindb.Username) && x.password.Equals(logindb.password)).FirstOrDefault();
+                if (data != null) 
+                {
+                    ViewBag.check = "Login Successfully";
+                    Session["Username"] = logindb.Username;
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ViewBag.check = "Login Failed";
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout() 
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
